5eca4a2168c3dff49ec3cea8fdafeead0042dbdc
divyadangroshiya <divya.dangroshiya21@gmail.com>
2023-05-29 07:12:11 +0530

    first commit

---
+++ src/main/java/com/employee/SpringbootEmployeeManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeeManagementBackendApplication.class, args);
+	}
+
+}
